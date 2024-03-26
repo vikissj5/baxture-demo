7397eb99677182f9a10c031d212d04b03d17f9fa
vikissj5 <vikissj5@gmail.com>
2024-03-27 02:40:26 +0530

    breakpoints were not accessible on initial attemps so achieve responsiveness using window element(not ideal)

--- app/page.tsx
+++ app/page.tsx
@@ -7,36 +7,50 @@ import { useEffect, useState } from "react";
 
 export default function HomePage() {
   const [cards, setCards] = useState([]);
+  const [screenSize, setScreenSize] = useState<number>(window.innerWidth); // Initialize with the initial screen width
+
   useEffect(() => {
     async function getCards() {
       let data = await getData();
       setCards(data);
     }
+
     getCards();
-  }, [])
+
+    const handleResize = () => {
+      setScreenSize(window.innerWidth); // Update screen size on resize
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // Cleanup on component unmount
+    };
+  }, []);
 
   const deleteItem = (id: number) => {
     const newCards = cards.filter((d: any) => id !== d.id);
     setCards(newCards);
-  }
+  };
+
   return (
     <>
-      <Grid overflow="hidden" style={{
-        margin: '20px'
-      }}>
-        {cards.map((d: any) => (<GridCol span={3} key={d.id} >
-          <Cards
-            title={d.name}
-            email={d.email}
-            domain={d.website}
-            phone={d.phone}
-            id={d.id}
-            deleteAction={() => deleteItem(d.id)}
-          />
-        </GridCol>))}
+      <Grid overflow="hidden" style={{ margin: '20px' }}>
+        {cards.map((d: any) => (
+          <GridCol span={screenSize <= 768 ? 12 : screenSize <= 1024 ? 6 : 3} key={d.id}>
+            <Cards
+              title={d.name}
+              email={d.email}
+              domain={d.website}
+              phone={d.phone}
+              id={d.id}
+              deleteAction={() => deleteItem(d.id)}
+            />
+          </GridCol>
+        ))}
       </Grid>
-
     </>
   );
 }
+
 
